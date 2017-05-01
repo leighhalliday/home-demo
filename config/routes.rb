c6250620960c98be13da544e55c495a753d31b46
Leigh Halliday <leighhalliday@gmail.com>
2017-05-01 06:33:50 -0400

    Removing react on rails example from app

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'hello_world', to: 'hello_world#index'
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "houses#index"
+  devise_for :users
 
   resources :houses, only: [:new, :show, :create] do
     collection do
